afcb1fb172ba9768203183829efe36dafea96990
Dimitri Gerson <dimitri.gerson@gmail.com>
2020-11-12 22:33:31 +0100

    création du projet avec tous les fichiers de base.

---
+++ tools_php/entete.php
@@ -0,0 +1,64 @@
+
+    
+<div class="container-fluid mb-0 ">
+
+	<div class="row alert alert-secondary mb-0">
+    <!--<img src="https://www.tamron-usa.com/IO/IndOp/assets/style_bridge.jpg" class="img-responsive" /> -->
+
+    <div class="col-xl-4 col-lg-5 col-md-6 order-xl-2">
+      <!--<table >
+        <tbody>
+          <tr>
+            <td class="align-baseline" ></td>
+            <td class="align-middle mb-0"> -->
+              <h1 class="text-center">Ceci est mon site boite à outils pour le web</h1>
+              
+                <div class="text-center"><a class="btn btn-warning text-primary" href="page_JavaScript.php">Page Javascript</a></div>
+           <!-- </td>
+          </tr>
+        </tbody>
+      </table> -->
+    </div>
+
+    <div class="col-xl-4 col-lg-3 col-md-3 d-sm-none d-none d-sm-block d-md-block order-xl-1" > 
+      <div class="d-none d-sm-block d-sm-none d-md-block">
+        <h2 class="text-center mb-2">Page en Python</h2>
+      </div>
+      <div class="d-md-none d-lg-block d-lg-none d-xl-block">
+        <p class="text-center paragrapheentete pt-1 mb-2" id="tete">Son rôle est de me permettre de répertorier les outils dont j'ai besoin pour la réalisation de sites.</p>
+      </div>
+      <div class="d-none d-lg-block d-xl-none"><p class="text-center paragrapheentete pt-1 mb-2" id="tete">Son rôle est de me permettre de répertorier tous les outils.</p>
+      </div>
+
+      <div class="d-none d-md-block d-lg-none">
+        <p class="text-center paragrapheentete pt-1 mb-2" id="tete">Les outils en Python.</p>
+      </div>
+        <div class="text-center d-none d-sm-block d-sm-none d-md-block pt-2"><a class="btn btn-warning text-primary" href="http://dimitrigerson.fr">Page Python</a></div>
+    </div> 
+
+    <div class="col-lg-4 col-md-3 d-sm-none d-none d-sm-block d-md-block order-xl-3">
+      <div class="d-none d-sm-block d-sm-none d-md-block">
+        <h2 class="text-center mb-3" id="titrephp">Page en PHP</h2>
+      </div>
+      <div>
+        <p class="mb-2 d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block paragrapheentete text-center" id="tailleparagPHP">Il utilise les include pour les parties communes sur chaque page. L'ensemble du site est responsive.</p>
+        <p class="d-none d-md-block d-lg-none  paragrapheentete text-center mb-2" id="tailleparagPHP">L'ensemble du site est responsive (include php).</p>
+      </div>
+      <!-- 
+      <table >
+        <tbody >
+          <tr >
+            <td class="align-baseline" ></td>
+            <td class="align-bottom mb-0">
+              <p class="text-center mb-0 paragrapheentete" id="tete">Il utilise les include pour les parties commune sur chaque page. L'ensemble du site est responsive.</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    -->
+      <div class="text-center d-none d-md-block d-lg-none"><a class="btn btn-warning text-primary" href="page_JavaScript.php">Page PHP</a></div>
+      <div class="text-center d-md-none d-lg-block pt-1"><a class="btn btn-warning text-primary" href="page_JavaScript.php">Page PHP</a></div>
+      </div> 
+    </div>
+  </div>
+</div
