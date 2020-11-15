c8891fc56eab80cd89d3b4b91a3b011f29485504
Dimitri Gerson <dimitri.gerson@gmail.com>
2020-11-15 21:20:50 +0100

    un commit pour futur nouvelle version

--- tools_php/fin_html.php
+++
@@ -1,9 +0,0 @@
-	<!-- jQuery (necessary for Bootstrap's Javascript plugin) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<!--include all compiled plugins (below), ir include individual files as needed -->
-	<script src="dist/js/bootstrap.js"></script>
-	<script type="text/javascript" src="date_heure.js"></script>
-    <script type="text/javascript" src="afficher_cacher.js"></script>
-    <script type="text/javascript" src="changerbouton.js"></script>
-	</body>
-</html>
