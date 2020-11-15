fd1fb7f9e5b604038efbc1c27362a0eb0f331961
Dimitri Gerson <dimitri.gerson@gmail.com>
2020-11-15 21:02:36 +0100

    refonte du site.

---
+++ site/tools_php/menu.php
@@ -0,0 +1,55 @@
+
+<nav class="navbar navbar-dark bg-dark navbar-expand-sm navbar-expand-md navbar-expand-lg">
+    <div class="container-fluid ">
+         <!-- <div class="navbar-header">
+        logo de l'entreprise
+        </div> -->
+           <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#partie-reduite" aria-controls="partie-reduite" aria-label="Toggle navigation" aria-expanded="false" > <!-- -->
+                <span class="navbar-toggler-icon"></span> 
+                <!--<span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span> -->
+            </button>
+       
+
+        <div class="collapse navbar-collapse" id="partie-reduite">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active"><a class="nav-link" href="test.php" class="nav-link" id="menu">PHP</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.html" class="nav-link">HTML</a></li>
+                <li class="nav-item"><a class="nav-link" href="css.php" class="nav-link" id="menu">CSS</a></li>
+                <li class="nav-item"><a class="nav-link" href="page_JavaScript.php" class="nav-link">JavaScript</a></li>
+                <li class="nav-item"><a class="nav-link" href="monnouveau.php" class="nav-link">Téléchargements</a></li>
+                
+
+            </ul>
+            <a class="btn btn-primary my-2 my-sm-0" href="index_2.php">Retour au menu</a>
+        </div>
+    <!--</div>-->
+</nav>
+
+     <!--       <div class="row">
+
+   
+	<div class="col-md-1">
+	<a href="test.php" id="menu">PHP</a>
+	</div>
+    <div class="col-md-1">
+    <a href="index.html" id="menu">HTML</a>
+    </div>
+    <div class="col-1">
+    <a href="css.php" id="menu">CSS</a>
+    </div>
+    <div class="col-1">
+    <a href="page_JavaScript.php">JavaScript</a>
+    </div>
+    <div class="col-2">
+    <a href="monnouveau.php">Répertoire des fichiers</a>
+    </div>
+    <div class="col-4"></div>
+    <div class="col-2">
+    <a href="index_2.php">Retour au menu</a>
+    </div> -->
+
+       
+            
